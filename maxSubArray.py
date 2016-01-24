9f1e94eaa0a43c426cd2f4477d2e2f8cee8c7cc8
andrew310 <andrewbrown600@gmail.com>
2016-01-23 23:17:45 -0800

    Lot of changes
    still working on this, but we needed output to a text file. I have that
    working...

--- maxSubArray.py
+++ maxSubArray.py
@@ -1,22 +1,7 @@
+from ast import literal_eval
 import sys
-import timeit
+import time
 __author__ = 'Andrew, Myles, Robert'
-
-with open("MSS_Problems.txt") as inFile:
-    testArray = ''.join(inFile.readline())
-    testArray = testArray.replace("[", "").replace(",", "").replace("]", "").split()
-    testArray = map(int, testArray)
-
-if len(sys.argv) > 1:
-    algorithmToRun = sys.argv[1]
-else:
-    print("Please specify algorithm you want to use as an argument.")
-    print("eg. python maxSubArray.py <Algorithm Number>")
-    print("Algorithm #1 - Enumeration")
-    print("Algorithm #2 - Better Enumeration")
-    print("Algorithm #3 - Divide and Conquer")
-    print("Algorithm #4 - Linear-Time")
-    sys.exit()
 
 def maxSubArrayEnum(numList):
     start, end, maxSum = 0, 0, 0
@@ -34,7 +19,7 @@ def maxSubArrayEnum(numList):
     print "Max Sum:",
     print maxSum
     print "Runtime:",
-    return maxSum
+    return (maxSum, maxSumArray)
 
 def maxSubArrayEnum2(numList):
     maxSum, tempSum = 0, 0
@@ -53,7 +38,11 @@ def maxSubArrayEnum2(numList):
     print "Max Sum:",
     print maxSum
     print "Runtime:",
-    return maxSum
+    return (maxSum, maxSumArray)
+
+def algo3Start(list):
+
+    return divideConquer(list, 0, len(list)-1)
 
 def divideConquer(array, left, right):
     if (left == right): # if only one element
@@ -65,16 +54,17 @@ def divideConquer(array, left, right):
     leftMax = 0
     rightMax = 0
     tempMax = 0
+
     # combined funtions maxPrefix and maxSuffix from pseudocode here
     for i in range(middle, -1, -1): # backwards loop starting at middle to 0, find max sub array of left sub
         tempMax += array[i]
-
         if(tempMax > leftMax):
             leftMax = tempMax
 
     tempMax = 0
     for i in range(middle+1, right+1): # loop for right sub, find max array of right sub
         tempMax += array[i]
+        startRight = middle+1
         if(tempMax > rightMax):
             rightMax = tempMax
 
@@ -90,47 +80,39 @@ def maxSubArrayLT(numList):
             bestSum = localSum
             bestStart = localStart
             bestEnd = i
-        if localSum <= 0:
+        if localSum <= 0: # if localSum dips below 0
             localSum = 0
             localStart = i + 1
 
     maxSumArray = numList[bestStart:bestEnd+1]
-    print "Max SubArray:"
-    print maxSumArray, "\n"
+    #print "Max SubArray:"
+    #print maxSumArray, "\n"
     print "Max Sum:",
     print bestSum
-    print "Runtime:",
+    #print "Runtime:",
+    return (bestSum, maxSumArray)
 
-    return bestSum
 
-def printResult(algorithm):
-        print algorithm(testArray)
-def printResultDC(algorithm, start, end):
-    print algorithm(testArray, start, end)
+#########FILE INPUT AND OUPUT##########
+inFile = open("MSS_Problems.txt", "r")
+outFile = open("MSS_Results.txt", "w")
+appendFile = open("MSS_Results.txt", "a")
 
-if (algorithmToRun == "Algorithm 1" or algorithmToRun == "1"):
-    print("Using enumeration algorithm:")
-    # printResult(maxSubArrayEnum)
-    t1 = timeit.Timer( "maxSubArrayEnum(testArray)", "from __main__ import maxSubArrayEnum, testArray")
-    print t1.timeit(number=1)
-elif (algorithmToRun == "Algorithm 2" or algorithmToRun == "2"):
-    print("Using better enumeration algorithm:")
-    # printResult(maxSubArrayEnum2)
-    t1 = timeit.Timer( "maxSubArrayEnum2(testArray)", "from __main__ import maxSubArrayEnum2, testArray")
-    print t1.timeit(number=1)
-elif (algorithmToRun == "Algorithm 3" or algorithmToRun == "3"):
-    print("Using divide and conquer algorithm:")
-    print "Max Sum:",
-    printResultDC(divideConquer, 0, len(testArray) - 1)
-    print "Runtime:",
-    t1 = timeit.Timer( "divideConquer(testArray, 0, len(testArray) -1)", "from __main__ import divideConquer, testArray")
-    print t1.timeit(number=1)
-elif (algorithmToRun == "Algorithm 4" or algorithmToRun == "4"):
-    print("Using Linear-Time algorithm:")
-    # printResult(maxSubArrayLT)
-    t1 = timeit.Timer( "maxSubArrayLT(testArray)", "from __main__ import maxSubArrayLT, testArray")
-    print t1.timeit(number=1)
+numbers = inFile.read()
 
-print ("\nArray Input:"),
-print testArray
-print ("\n")
+
+def runAlgorithm(algorithm, note):
+    outFile.write("\n")
+    outFile.write(note)
+    outFile.write("\n")
+    for line in numbers.splitlines():
+        lst = literal_eval(line)
+        outFile.write(str(algorithm(lst)))
+        outFile.write("\n")
+
+
+runAlgorithm(maxSubArrayEnum, "Enumeration Results:")
+runAlgorithm(maxSubArrayEnum2, "Iteration Results:")
+runAlgorithm(algo3Start, "Divide & Conquer Results:")
+runAlgorithm(maxSubArrayLT, "Linear Results:")
+
